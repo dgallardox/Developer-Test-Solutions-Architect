2e144fce375d855a2be3f43d7eae97aa6bd9f092
Diego J.  Gallardo <101141475+dgallardox@users.noreply.github.com>
2024-08-26 18:06:43 +0000

    updated revalidation to 900

--- faust-app/pages/[...wordpressNode].js
+++ faust-app/pages/[...wordpressNode].js
@@ -5,7 +5,7 @@ export default function Page(props) {
 }
 
 export function getStaticProps(ctx) {
-  return getWordPressProps({ ctx });
+  return getWordPressProps({ ctx, revalidate: 900 });
 }
 
 export async function getStaticPaths() {
